a1af3974ec4c46af9522eea8b303232ccd0ab0f6
AndriychukAnna <andriychykal98@gmail.com>
2021-07-28 09:16:08 +0300

    section prodacts first commit

---
+++ src/js/products-modal-2.js
@@ -0,0 +1,17 @@
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector('[data-modal-open-2]'),
+      closeModalBtn: document.querySelector('[data-modal-close-2]'),
+      modal: document.querySelector('[data-modal-2]'),
+    };
+  
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+    
+      
+    function toggleModal() {
+      refs.modal.classList.toggle('modal__hidden');
+    }
+  })();
+
+  
